20ffc7608b58380a5e7c6d3ccd1fe036bbce5ae6
Remco Uittenbogerd <remco@cs.ualberta.ca>
2014-03-13 14:37:53 -0600

    Right when I thought I could do it all in one commit.... Forgot to add myself to the Copyright.

--- sockets.py
+++ sockets.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 # coding: utf-8
+# Copyright (c) 2014 Remco Uittenbogerd
 # Copyright (c) 2013-2014 Abram Hindle
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
